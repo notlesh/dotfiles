5b3cec611cd044670b6d58a31b9c68d583cd857b
Stephen Shelton <stephen@stephenashelton.com>
2019-10-26 19:06:06 -0600

    Add some new-linux-box setup scripts

---
+++ .bin/setup/install_packages.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# just gonna leave this here...
+# https://www.wezm.net/technical/2019/10/useful-command-line-tools/
+
+# headless packages
+HEADLESS_PACKAGES="
+neovim
+powertop
+cmake build-essential
+keychain
+virtualbox
+python-pip
+ctags
+autofs
+mutt
+tmux
+ssh
+npm
+resolvconf
+smartmontools
+irssi
+"
+
+# non-headless packages
+NON_HEADLESS_PACKAGES="
+fonts-hack
+discord
+libreoffice
+xserver-xephyr
+gimp
+screenruler
+kmag
+gpick
+scrot
+"
+
+# personal machines
+PERSONAL_PACKAGES="
+mpd
+ncmpcpp
+mpc
+mumble
+"
+
+# for specific hardware
+HARDWARE_PAKCAGES="
+bluetooth 
+blueman
+nvme-cli
+"
+
+PACKAGES="$HEADLESS_PACKAGES $NON_HEADLESS_PACKAGES $PERSONAL_PACKAGES $HARDWARE_PAKCAGES"
+
+sudo apt install $PACKAGES
+
+CARGO_PACKAGES="du-dust eva chars"
+
+cargo install $CARGO_PACKAGES
