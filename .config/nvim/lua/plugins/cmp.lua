78554e486e39d8f331b78eaa4613e41be2254c04
Stephen Shelton <steve@brewcraft.org>
2024-04-20 14:58:03 -0600

    Add missing file

---
+++ .config/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,26 @@
+return {
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+		},
+		lazy = false,
+		config = function()
+			local cmp = require('cmp')
+			cmp.setup({
+				snippet = {},
+				window = {},
+				mapping = {},
+				sources = cmp.config.sources({
+					{ name = 'nvim_lsp' },
+					{ name = 'nvim_lsp' },
+				}),
+			})
+
+			local capabilities = require('cmp_nvim_lsp').default_capabilities()
+			require('lspconfig')['rust_analyzer'].setup {
+				capabilities = capabilities
+			}
+		end
+	}
+}
